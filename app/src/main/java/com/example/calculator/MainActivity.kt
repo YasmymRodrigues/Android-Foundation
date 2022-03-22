776c22ab00249a8b17f386fa3f3876e90c54803a
learngit <yasmymrdgs@gmail.com>
2022-03-22 15:09:12 +0000

    Creating the first buttons and Log.i in the Main

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = MainActivity::class.java.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
+        viewId.setOnClickListener {
+            Log.i(TAG,"motorstart")
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
